1b8c26dcb09e6d17ef024f6ee57d6e142fe6331c
OSAMA <osama@Osama-MacBook-Air.local>
2022-08-13 20:44:09 +0530

    react_food_app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setCartIshown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIshown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIshown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import mealsimage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsimage} alt="food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
